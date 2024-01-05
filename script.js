3bb675f252a893cfdd368ce84db79fc7e3a52407
Toby Le <toby@outcubator.com>
2024-01-05 11:06:55 +0700

    Update overlay size

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function setVideoDimension() {
   canvasElement.height = actualHeight;
   webcamOverlay.setAttribute("viewBox", `0 0 ${window.innerWidth} ${window.innerHeight}`)
   webcamOverlayOval.setAttribute("rx", ratio > 1 ? actualWidth * 0.6 : actualHeight * 0.8 * 0.35)
-  webcamOverlayOval.setAttribute("ry", ratio > 1 ? actualWidth * 0.8 * 0.6 : actualHeight * 0.35)
+  webcamOverlayOval.setAttribute("ry", ratio > 1 ? actualWidth / 0.8 * 0.6 : actualHeight * 0.35)
 }
 
 function calculateHeadPose(results) {
